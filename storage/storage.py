8295f81ad41f865741cc2c78d627069419f5a148
Kevin X <tnkgavas363@gmail.com>
2023-11-20 22:47:23 -0800

    Made changes to config files

--- storage/storage.py
+++ storage/storage.py
@@ -14,19 +14,35 @@ from pykafka import KafkaClient
 from pykafka.common import OffsetType
 from threading import Thread
 import time
+import os
 
-with open('app_conf.yml', 'r') as f:
-     app_config = yaml.safe_load(f.read())
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+    
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+with open(app_conf_file, 'r') as f:
+    app_config = yaml.safe_load(f.read())
+
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+
+logger = logging.getLogger('basicLogger')
 
 DB_ENGINE = create_engine(f"mysql+pymysql://{app_config['datastore']['user']}:{app_config['datastore']['password']}@{app_config['datastore']['hostname']}:{app_config['datastore']['port']}/{app_config['datastore']['db']}")
 Base.metadata.bind = DB_ENGINE
 DB_SESSION = sessionmaker(bind=DB_ENGINE)
 
-logger = logging.getLogger('basicLogger')
 
-with open('log_conf.yml', 'r') as f:
-    log_config = yaml.safe_load(f.read())
-    logging.config.dictConfig(log_config)
+
+
 
 
 def get_item_usage(start_timestamp, end_timestamp):
